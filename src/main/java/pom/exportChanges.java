27f46d3266a0986f6d83ca5901e592ee9c1e0f2d
yashwanthkumarkc09 <yashwanthkc09@gmail.com>
2019-11-29 12:35:13 +0530

    New Checkin,Comments by Admin a Test Sage User

---
+++ src/main/java/pom/exportChanges.java
@@ -0,0 +1,50 @@
+
+package pom;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.support.FindAll;
+
+import org.openqa.selenium.support.FindBy;
+
+import org.openqa.selenium.support.PageFactory;
+
+
+
+import reuseablePackage.feature.AllActions;
+
+
+
+public class exportChanges {
+
+// public WebDriver driver=null;
+
+// public exportChanges (WebDriver driver)
+// 	{
+// 		this.driver = driver;
+// 		PageFactory.initElements(AllActions.driver, this);        // Reusable is a Reusable class where we initalize Web Driver ( public static WebDriver driver =null)
+//     }
+    
+ThreadLocal<WebDriver> driver;
+	 
+	public exportChanges (ThreadLocal<WebDriver> driver)
+	{
+		this.driver = driver;
+		PageFactory.initElements(driver.get(), this);    
+	}	
+
+
+@FindBy(xpath="//input[@id='userName']")public WebElement userName;
+
+@FindBy(xpath="//input[@id='password']")public WebElement userPassword;
+
+@FindBy(id="loginBtn")public WebElement userLogin;
+
+@FindBy(xpath="//a[@class='btn btn-primary dropdown-toggle blue-btn']")public WebElement userLogout;
+//pomStart
+	
+
+
+}
---
+++ src/main/java/reuseablePackage/reuseFunction/meHappyLoginClass.java
@@ -0,0 +1,85 @@
+
+package reuseablePackage.reuseFunction;
+import pom.exportChanges;
+import reuseablePackage.feature.AllActions;
+import org.openqa.selenium.WebDriver;
+public class meHappyLoginClass{
+	
+	public static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+	
+	public meHappyLoginClass(ThreadLocal<WebDriver> driver) {
+
+		this.driver=driver;
+	}
+	AllActions actions = new AllActions();
+String user;
+String password;
+boolean check;
+public void meHappyLogin(String user,String password) throws Exception
+{
+actions.EnterApplicationURL("http://web.meehappy.com:9090/login");
+actions.EnterData(new exportChanges(driver).userName,user);
+actions.EnterData(new exportChanges(driver).userPassword,password);
+actions.Click(new exportChanges(driver).userLogin);
+
+
+
+
+
+
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// package reuseablePackage.feature;
+
+// import java.util.concurrent.TimeUnit;
+
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.WebElement;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.chrome.ChromeOptions;
+// import java.net.MalformedURLException;
+
+// public class meHappyLoginClass extends  AllActions {
+// 	public static WebDriver driver;
+	
+// 	AllActions actionObject = new AllActions();
+	
+// 	}  
+// }
